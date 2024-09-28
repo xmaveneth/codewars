d43d5d59cb1056ca795b025581964cce0ab1864e
xmaven <kirillgolovanov777@gmail.com>
2024-09-28 17:11:03 +0300

    Add cakes.py

---
+++ tasks/cakes.py
@@ -0,0 +1,10 @@
+def cakes(recipe, available):
+    data = []
+    for i in recipe:
+        if i not in available:
+            return 0
+        data.append(available[i] // recipe[i])
+    return min(data)
+
+
+# https://www.codewars.com/kata/525c65e51bf619685c000059
