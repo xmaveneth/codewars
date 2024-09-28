7011324b8f9ec0199b52aaecd3c3587e485cf88c
xmaven <kirillgolovanov777@gmail.com>
2024-09-28 17:04:19 +0300

    Optimization of max_sequence.py

--- tasks/max_sequence.py
+++ tasks/max_sequence.py
@@ -1,15 +1,12 @@
 def max_sequence(arr):
     result = 0
-    for i in range(len(arr)):
-        if arr[i] < 0:
-            continue
-        summ = arr[i]
-        for j in range(i + 1, len(arr)):
-            summ += arr[j]
-            if summ > result:
-                result = summ
+    cur_result = 0
+    for el in arr:
+        cur_result += el
+        result = max(result, cur_result)
+        cur_result = max(cur_result, 0)
     return result
 
 
 # https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c
-# Пока не проходят некоторые тесты? Думаю над оптимизацией
+# Удалось оптимизировать алгоритмом Кадана. Сложность O(n)
